ef0c1a2712326a52f21ff8cc76d569b41ff9dd57
Patton Doyle <pattondoyle@gmail.com>
2013-02-14 15:04:17 -0500

    Added the ability to send commands to the balloon

--- balloon-data.cpp
+++ balloon-data.cpp
@@ -22,24 +22,43 @@ using namespace std;
 /// Global Constants
 //////////////////////////////////////////////////////////////////////////////
 
-// Serial buffer size
+// Serial Buffer Size
 const int MAXBUF = 1000;
 
-// Maximum number of data points
+// Maximum Number of Data Points
 const int MAXPTS = 1000;
 
-// Maximum GPS data point size
+// Maximum GPS Data Point Size
 const int MAXGPSDAT = 50;
+
+// Maximum Command String Size
+const int MAXCMD = 100;
+
+// Command Delimeter
+const char CMDDLIM = '$';
+
+// Default Command
+/// !!!
 
 
 //////////////////////////////////////////////////////////////////////////////
 /// Internal Functions
 //////////////////////////////////////////////////////////////////////////////
 
-bool openFile(ofstream &file, const string &filenm);
+bool openDFile(ofstream &file, const string &filenm);
 // MODIFIES: file, cout
 // EFFECTS: Opens <file> at file <filenm> in append mode, prints an error and
-//		returns 0if the file does not open
+//		returns 0 if the file does not open
+
+bool openCFile(ifstream &file, const string &filenm);
+// MODIFIES: file, cout
+// EFFECTS: Opens <file> at file <filenm>, prints an error and returns 0 if
+//		the file does not open
+
+void promptParam(int &dlay, int &mapdlay);
+// MODIFIES: dlay, mapdlay
+// EFFECTS: Prompts for program refresh delay and map refresh delay and places
+//		 the values in dlay and mapdlay, respectively
 
 void openPort(int &comnum, int &baud);
 // MODIFIES: Sets comnum to the port that is opened and baud to its baudrate,
@@ -50,23 +69,35 @@ void openPort(int &comnum, int &baud);
 bool verifyPacket(const unsigned char *buff);
 // EFFECTS: verifies that the data packet is complete
 
+void writeHTML(const string &dfilenm, const int mapdlay, const int pkts);
+// MODIFIES: cout
+// EFFECTS: Writes the data in <dfilenm> to a map in GPSmap.html assuming
+//		<pkts> data packets with a delay of <mapdlay>
+
 void writeHeader(ofstream &maphtml, const int mapdlay);
 // REQUIRES: maphtml points to an open ofstream
 // MODIFIES: maphtml
 // EFFECTS: Prints the HTML header to the file for GPS mapping with refresh
 //		delay <mapdlay>
 
-void writePts(ofstream &maphtml, const string filenm, const int pkts);
-// REQUIRES: maphtml points to an open ofstream, filenm is the name of
+void writePts(ofstream &maphtml, const string &dfilenm, const int pkts);
+// REQUIRES: maphtml points to an open ofstream, dfilenm is the name of
 //		valid datafile with no more than <pkts> datapoints
 // MODIFIES: maphtml
-// EFFECTS: Write the the lat and lon points in the file <filenm> to the
+// EFFECTS: Write the the lat and lon points in the file <dfilenm> to the
 //		HTML of maphtml
 
 void writeEnd(ofstream &maphtml);
 // REQUIRES: maphtml points to an open ofstream
 // MODIFIES: maphtml
 // EFFECTS: Prints the HTML end to the file for the GPS mapping
+
+void sendCMD(const string &cfilenm, const int comnum);
+// REQUIRES: cfilenm is the name of a valid command file, comnum is the
+//		serial port in use
+// MODIFIES: cout
+// EFFECTS: Sends a valid command in <cfilenm> (see README) if it exists
+//		and sends a default command if not
 
 
 //////////////////////////////////////////////////////////////////////////////
@@ -75,36 +106,39 @@ void writeEnd(ofstream &maphtml);
 
 int main ()
 {
-	// Program header
+	// Program Header
 	cout << "\nBalloon Data - by the Space Whale team\nVersion 0.2.1"
 		<< "\n========================================\n";
 
 	// Prompt for Datafile and Open
 	ofstream datfile;
-	string filenm;
+	string dfilenm;
 	cout << "File to store incoming data:\n";
-	cin >> filenm;
-	while (!openFile(datfile, filenm)) {
+	cin >> dfilenm;
+	while (!openDFile(datfile, dfilenm)) {
 		cout << "File to store incoming data:\n";
-		cin >> filenm;
+		cin >> dfilenm;
 	}
 
 	// Write File Header and Save
 	datfile << "Begin New Data\n========================================\n";
 	datfile.close();
 
-	// Prompt for Program Refresh Rate
-	int dlay;
-	cout << "\nDelay between each data read (in seconds):\n";
-	cin >> dlay;
+	// Prompt for Command File
+	ifstream cmdfile;
+	string cfilenm;
+	cout << "File to send commands to balloon:\n";
+	cin >> cfilenm;
+	while (!openCFile(cmdfile, cfilenm)){
+		cout << "File to send commands to balloon:\n";
+		cin >> cfilenm;
+	}
 
-	// Prompt for HTML Refresh Rate
-	int mapdlay;
-	cout << "\nDelay between each HTML refresh (in seconds):"
-		<< "\n[Zero for never]\n";
-	cin >>mapdlay;
+	// Prompt for Other Parameters
+	int dlay, mapdlay;
+	promptParam(dlay, mapdlay);
 
-	// Prompt for and open serial port
+	// Prompt for and Open Serial Port
 	int comnum, baud;
 	openPort(comnum, baud);
 
@@ -130,7 +164,7 @@ int main ()
 
 		// Write Data to File
 		cout << "Writing to file ..... ";
-		if (openFile(datfile, filenm)) {
+		if (openDFile(datfile, dfilenm)) {
 
 			// Write Individual Bytes
 			for (int i = 0; i < datnum; ++i) {
@@ -143,25 +177,16 @@ int main ()
 		}
 
 		// Make HTML File
-		ofstream maphtml;
-		maphtml.open("GPSmap.html", ios_base::trunc);
-		if (!maphtml.good()) {
-			cout << "\nError creating html file.\n";
-		}
-		writeHeader(maphtml, mapdlay);
+		writeHTML(dfilenm, mapdlay, pkts);
 
-		// Read Data from File and Place on Map
-		writePts(maphtml, filenm, pkts);
-
-		// Write HTML File End and Close
-		writeEnd(maphtml);
-		maphtml.close();
+		// Send Command to Balloon
+		//sendCMD();
 
 		// Pause For Dlay Seconds
 		cout << "Waiting ...\n";
 		#ifdef __GNUC__
 	/// Commented out for compatability with MinGW
-		//	sleep(dlay);
+			sleep(dlay);
 		#endif
 		#ifdef _WIN32
 			_sleep(dlay * 1000);
@@ -177,12 +202,12 @@ int main ()
 /// Internal Function Implementations
 //////////////////////////////////////////////////////////////////////////////
 
-bool openFile(ofstream &file, const string &filenm)
+bool openDFile(ofstream &file, const string &filenm)
 {
 	// Open filenm
 	file.open(filenm.c_str(), ios_base::app);
 	if (!file.good()) {
-		cout << "Could not open datafile.\n";
+		cout << "Could not open data file.\n";
 		return false;
 	}
 
@@ -190,23 +215,48 @@ bool openFile(ofstream &file, const string &filenm)
 	return true;
 }
 
+bool openCFile(ifstream &file, const string &filenm)
+{
+	// Open filenm
+	file.open(filenm.c_str());
+	if (!file.good()) {
+		cout << "Could not open command file.\n";
+		return false;
+	}
+
+	// Indicate Success
+	return true;
+}
+
+void promptParam(int &dlay, int &mapdlay)
+{
+	// Prompt for Program Refresh Rate
+	cout << "Delay between each data read (in seconds):\n";
+	cin >> dlay;
+
+	// Prompt for HTML Refresh Rate
+	cout << "Delay between each HTML refresh (in seconds):"
+		<< "\n[Zero for never]\n";
+	cin >>mapdlay;
+}
+
 void openPort(int &comnum, int &baud)
 {
 	// Prompt for Port
-	cout << "\nComport to open:\n[USB ports start at 16 for Linux]\n";
+	cout << "Comport to open:\n[USB ports start at 16 for Linux]\n";
 	cin >> comnum;
 
 	// Prompt for Baudrate
-	cout << "\nBaudrate for the comport:\n";
+	cout << "Baudrate for the comport:\n";
 	cin >> baud;
 
 	// Attempt to Open Port
 	while (RS232_OpenComport(comnum, baud)) {
 
 		// Ask for New Values
-		cout << "\nError opening the port. Enter new port number:\n";
+		cout << "Error opening the port. Enter new port number:\n";
 		cin >> comnum;
-		cout << "\nBaudrate for the comport:\n";
+		cout << "Baudrate for the comport:\n";
 		cin >> baud;
 	}
 }
@@ -215,6 +265,24 @@ bool verifyPacket(const unsigned char *buff)
 {
 	cout << "Success.\n";
 	return true;
+}
+
+void writeHTML(const string &dfilenm, const int mapdlay, const int pkts)
+{
+	// Make HTML File and Write Header
+	ofstream maphtml;
+	maphtml.open("GPSmap.html", ios_base::trunc);
+	if (!maphtml.good()) {
+		cout << "\nError creating html file.\n";
+	}
+	writeHeader(maphtml, mapdlay);
+
+	// Read Data from File and Place on Map
+	writePts(maphtml, dfilenm, pkts);
+
+	// Write HTML File Ending and Close
+	writeEnd(maphtml);
+	maphtml.close();
 }
 
 void writeHeader(ofstream &maphtml, int mapdlay)
@@ -237,11 +305,11 @@ void writeHeader(ofstream &maphtml, int mapdlay)
 		<< "\n		lats = [";
 }
 
-void writePts(ofstream &maphtml, const string filenm, int pkts)
+void writePts(ofstream &maphtml, const string &dfilenm, int pkts)
 {
 	// Open Datafile
 	ifstream gpsdat;
-	gpsdat.open(filenm.c_str());
+	gpsdat.open(dfilenm.c_str());
 	if (!gpsdat.good()) {
 		cout << "\nCould not open data file.\n";
 		return;
@@ -290,3 +358,35 @@ void writeEnd(ofstream &maphtml)
 		<< "\n\n	    map.setCenter(location);\n	  }\n		</script>\n	</head>\n	<body onload=\"initialize()\">"
 		<< "\n		<div id=\"map_canvas\" style=\"width:100%; height:100%\"></div>\n	</body>\n</html>";
 }
+
+void sendCMD(const string &cfilenm, const int comnum)
+{
+	// Open Command File
+	ifstream cmdfile;
+	openCFile(cmdfile, cfilenm);
+
+	// Read Line into String
+	string cmd;
+	cmdfile.getline(cmd, MAXCMD);
+
+	// Send Command if Valid
+	const int cmd_size = cmd.size();
+	if (cmd[cmd_size - 1] == CMDDLIM) {
+
+		// Transmit Command
+		cout << "Sending command \"" << cmd << "\" ..... ";
+		if (RS232_SendBuf(comnum, cmd.c_str(), cmd_size) != cmd_size)
+			cout << "Error sending command.\n";
+		else
+			cout << "Success.\n";
+
+	// Default Command
+	} else {
+
+		/// Default Command !!!
+	}
+
+	// Close Command File
+	cmdfile.close();
+}
+
