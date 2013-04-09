9e4db2054812cf3969087b7d5ff5654096cd4255
Patton Doyle <pattondoyle@gmail.com>
2013-04-09 18:25:59 -0400

    Autoversioning Using the Code::Blocks Plugin
    This file will be updated with each new version.

---
+++ version.h
@@ -0,0 +1,27 @@
+#ifndef VERSION_H
+#define VERSION_H
+
+namespace AutoVersion{
+	
+	//Software Status
+	static const char STATUS[] = "Beta";
+	static const char STATUS_SHORT[] = "b";
+	
+	//Standard Version Type
+	static const long MAJOR = 0;
+	static const long MINOR = 4;
+	static const long BUILD = 4;
+	static const long REVISION = 15;
+	
+	//Miscellaneous Version Types
+	static const long BUILDS_COUNT = 445;
+	#define RC_FILEVERSION 0,4,4,15
+	#define RC_FILEVERSION_STRING "0, 4, 4, 15\0"
+	static const char FULLVERSION_STRING[] = "0.4.4.15";
+	
+	//These values are to keep track of your versioning state, don't modify them.
+	static const long BUILD_HISTORY = 0;
+	
+
+}
+#endif //VERSION_H
